9ea168777139c2ffc9a032d8a150638e9a59b375
Md Rahimuddin <rahimuddinmd086@gmail.com>
2026-07-07 22:31:03 +0530

    Fix production WebSocket CORS handshake by removing withCredentials on socket client

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -5,6 +5,5 @@ const socketUrl = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export const socket = io(socketUrl, {
   autoConnect: false,
-  withCredentials: true,
   transports: ["websocket"]
 });
